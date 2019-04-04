cdd94452a81a79565a31ec4bea11d1d45ab923a0
Ajit A Sharma <ajit.sharma@bitstreet.co.in>
2019-04-04 14:40:16 +0530

    MyFragment

--- myplibrary/src/main/java/com/example/myplibrary/MyLibraryActivity.java
+++ myplibrary/src/main/java/com/example/myplibrary/MyLibraryActivity.java
@@ -4,17 +4,33 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MyLibraryActivity extends AppCompatActivity {
 
     public String name;
     public String age;
+    TextView txtCnt;
+    Button btnClick;
+    int cnt = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_lib);
+
+        txtCnt = findViewById(R.id.txtCnt);
+        btnClick = findViewById(R.id.btnClick);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCnt.setText(Integer.toString(cnt++));
+            }
+        });
 
     }
     public void MyToast(Context context){
