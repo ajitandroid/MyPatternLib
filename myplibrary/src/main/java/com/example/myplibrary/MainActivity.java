05ee919dd0c37bf91e8190661370e5a83ef1d3f7
Ajit A Sharma <ajit.sharma@bitstreet.co.in>
2019-04-04 12:46:28 +0530

    ToastMethod

--- myplibrary/src/main/java/com/example/myplibrary/MainActivity.java
+++ myplibrary/src/main/java/com/example/myplibrary/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myplibrary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "This is demo toast", Toast.LENGTH_SHORT).show();
+
+    }
+    private void MyToast(Context context){
+        Toast.makeText(context, "This is demo toast", Toast.LENGTH_SHORT).show();
     }
 }
